4a21a21e185676ea59636163f1356fbe71fb63e6
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2024-10-04 01:24:41 +0530

    menu start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Lamp from "./lamp/page"
 import Hero from "./Hero/page"
+import Nav from "../components/header/index"
 
 export default function Home() {
   return (
     <>
+      <Nav />
       <Hero />
       {/* <Lamp /> */}
     </>
